f992a531b1903c8445a46bd4fb3f56fe6d59acef
Zeworxx <zeworxx@gmail.com>
2023-11-02 12:58:20 +0100

    add translation for todolist comp

--- client/FullJsProject/src/components/todolist/ToDoList.vue
+++ client/FullJsProject/src/components/todolist/ToDoList.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+import type { UserData } from '@/models/app.model';
+import { type Ref, ref } from 'vue';
 
+const userData: Ref<UserData> = ref({
+    firstName: "Baptiste"
+})
 </script>
 
 <template>
-    <div class="p-4 sm:ml-64">
-        <span>a</span>
+    <div class="p-4">
+        <div>
+            <h1 class="flex items-center text-5xl font-extrabold dark:text-white">{{ $t('app.todolist.welcome-title', {
+                name:
+                    userData.firstName
+            }) }}</h1>
+            <h2 class="ml-1 text-4xl font-semibold text-gray-300 dark:text-gray-400">{{ $t('app.todolist.second-title') }}
+            </h2>
+            <div class="border border-blue-200 border-solid mt-4 rounded"></div>
+        </div>
     </div>
 </template>
