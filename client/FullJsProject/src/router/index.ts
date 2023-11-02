e5a08822ace3256ae359665e61e45fd049b5788e
Zeworxx <zeworxx@gmail.com>
2023-11-02 16:00:07 +0100

    add routing

--- client/FullJsProject/src/router/index.ts
+++ client/FullJsProject/src/router/index.ts
@@ -1,9 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../components/HomePage.vue'
+import TodayTask from '../components/TodayTask.vue'
+import SevenDayTask from '../components/SevenDayTask.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
+    { path: '/', name: 'home', component: HomePage },
+    { path: '/today-task', name: 'today', component: TodayTask },
+    { path: '/seven-day-task', name: 'seven', component: SevenDayTask }
   ]
 })
 
