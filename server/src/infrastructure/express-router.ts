df78e8da4770930dd8060c73c3f0fc035fe7c150
Zeworxx <zeworxx@gmail.com>
2023-11-20 15:19:05 +0100

    configuire router

--- server/src/infrastructure/express-router.ts
+++ server/src/infrastructure/express-router.ts
@@ -2,14 +2,20 @@ import { Router } from "express";
 import { UserService } from "../user/user-service";
 import { UserController } from "../user/user-controller";
 import { UserRouter } from "../user/user-router";
+import { TodolistRouter } from "../todolist/user-router";
+import { TodolistController } from "../todolist/user-controller";
+import { TodolistServices } from "../todolist/user-service";
 
 export class ExpressRouter {
     router = Router();
     private userController!: UserController
     private userRouter!: UserRouter
+    private todolistRouter!: TodolistRouter
+    private todolistController!: TodolistController
 
     constructor(
         private userService: UserService,
+        private todolistService: TodolistServices
     ) {
         this.configureControllers();
         this.configureRouters();
@@ -18,14 +24,17 @@ export class ExpressRouter {
 
     private configureControllers(): void {
         this.userController = new UserController(this.userService);
+        this.todolistController = new TodolistController(this.todolistService);
     }
 
     private configureRouters(): void {
         this.userRouter = new UserRouter(this.userController);
+        this.todolistRouter = new TodolistRouter(this.todolistController)
     }
 
     private configureRoutes(): void {
         this.router.use('/user', this.userRouter.router);
+        this.router.use('/todolist', this.todolistRouter.router);
     }
 
 }
