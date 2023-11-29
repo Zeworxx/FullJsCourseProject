f9db935aa9a46dc5eb55a8e39300a1adfa657cad
Baptiste <b.girard@ttge.fr>
2023-11-29 18:30:58 +0100

    a

--- client/FullJsProject/src/components/Login.vue
+++ client/FullJsProject/src/components/Login.vue
@@ -17,7 +17,7 @@ function submitForm() {
       class="bg-white w-screen md:w-fit md:rounded-3xl md:p-4 md:flex-row flex-col-reverse flex items-center"
     >
       <div class="md:p-10">
-        <div class="w-[400px]">
+        <div class="w-[350px]">
           <div class="px-6 py-6 mb-4 md:p-0">
             <span class="text-[#242742] text-[40px] font-semibold md:font-bold md:text-[50px]">
               {{ $t('app.login.title') }}
