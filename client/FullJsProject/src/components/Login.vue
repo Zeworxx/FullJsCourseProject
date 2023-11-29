2bb2292f87c623c1e9e9a038afc4aee76626c06d
Baptiste <b.girard@ttge.fr>
2023-11-29 15:18:58 +0100

    add translation

--- client/FullJsProject/src/components/Login.vue
+++ client/FullJsProject/src/components/Login.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import Email from './login/email.vue'
-import Check from './login/test.vue'
+import FormLogin from './login/FormLogin.vue'
 
 const success = ref(false)
 const animate = ref('animate-pulse')
@@ -12,35 +11,31 @@ function submitForm() {
 </script>
 
 <template>
-      <div
-      class="body w-screen md:h-screen items-center md:justify-center flex bg-slate-600"
+  <div class="body w-screen md:h-screen items-center md:justify-center flex bg-[#27293F]">
+    <div
+      v-if="!success"
+      class="bg-white w-screen md:w-fit md:rounded-3xl md:p-4 md:flex-row flex-col-reverse flex items-center"
     >
-  <div
-    v-if="!success"
-    class="bg-white w-screen md:w-fit md:rounded-3xl md:p-4 md:flex-row flex-col-reverse flex items-center"
-  >
-    <div class="md:p-10">
-      <div class="px-6 py-6 md:p-0">
-        <span class="text-[#242742] text-[40px] font-semibold md:font-bold md:text-[50px]">
-          Stay updated !
-        </span>
+      <div class="md:p-10">
+        <div class="w-[400px]">
+          <div class="px-6 py-6 mb-4 md:p-0">
+            <span class="text-[#242742] text-[40px] font-semibold md:font-bold md:text-[50px]">
+              {{ $t('app.login.title') }}
+            </span>
+          </div>
+          <!-- <Check /> -->
+          <FormLogin @submit="submitForm" />
+        </div>
       </div>
-      <Check />
-      <Email @submit="submitForm" />
+      <div class="w-[450px] flex-shrink-0">
+        <img class="md:flex hidden h-[500px]" src="../assets/images/Checklist.jpg" alt="" />
+      </div>
+      <div class="flex items-center w-full">
+        <img class="md:hidden w-full flex" src="../assets/mobil.svg" alt="" loading="lazy" />
+      </div>
     </div>
-    <div class="w-[450px] flex-shrink-0">
-      <img
-        class="md:flex hidden h-[500px]"
-        src="../assets/images/Checklist.jpg"
-        alt=""
-      />
+    <div v-else>
+      <Success />
     </div>
-    <div class="flex items-center w-full">
-      <img class="md:hidden w-full flex" src="../assets/mobil.svg" alt="" loading="lazy" />
-    </div>
-  </div>
-  <div v-else>
-    <Success />
-  </div>
   </div>
 </template>
---
+++ client/FullJsProject/src/components/login/FormLogin.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const email = ref('')
+const password = ref('')
+const error = ref(false)
+const emit = defineEmits(['submit'])
+
+function submit() {
+  if (!email.value.length || !ValidateEmail(email.value)) {
+    error.value = true
+  } else {
+    error.value = false
+    emit('submit')
+  }
+  console.log(email.value)
+}
+function ValidateEmail(mail) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+    return true
+  }
+  return false
+}
+</script>
+
+<template>
+  <div class="grid gap-4 px-6 md:p-0 pb-6">
+    <form class="w-full grid gap-4" @submit.prevent="submit">
+      <div class="flex justify-between items-center">
+        <span class="text-sm font-semibold">{{ $t('app.login.form.email-label') }}</span>
+        <span class="text-[#ff6257] text-sm font-semibold" v-if="error">Valid email required</span>
+      </div>
+      <input
+        :class="error ? 'border-[#ff6257] bg-[#ff4a3d] bg-opacity-10' : 'border-[#9294a0]'"
+        v-model="email"
+        :placeholder="$t('app.login.form.email-placeholder')"
+        type="email"
+        class="w-full p-3 rounded border outline-none"
+      />
+      <div class="flex justify-between items-center">
+        <span class="text-sm font-semibold">{{ $t('app.login.form.password-label') }}</span>
+        <span class="text-[#ff6257] text-sm font-semibold" v-if="error">Valid email required</span>
+      </div>
+      <input
+        :class="error ? 'border-[#ff6257] bg-[#ff4a3d] bg-opacity-10' : 'border-[#9294a0]'"
+        v-model="password"
+        :placeholder="$t('app.login.form.password-placeholder')"
+        type="password"
+        class="w-full p-3 rounded border outline-none"
+      />
+      <!-- :class="index === 5 || index === 6 ? 'red-color' : ''" -->
+      <button type="submit" class="bg-[#242742] text-white font-semibold w-full rounded p-3">
+        {{ $t('app.login.form.submit-button') }}
+      </button>
+    </form>
+  </div>
+</template>
+
+<style></style>
