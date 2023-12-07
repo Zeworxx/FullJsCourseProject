7b17d83d23333ece9490931e2b41e5fb267c6d58
Zeworxx <zeworxx@gmail.com>
2023-12-07 22:49:39 +0100

    aded user id in todolist component

--- client/FullJsProject/src/components/todolist/ToDoList.vue
+++ client/FullJsProject/src/components/todolist/ToDoList.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import type { UserData } from '@/models/app.model'
 import { type Ref, ref, onMounted } from 'vue'
-import { TodolistServices } from '../services/TodolistServices.vue';
 import { UserServices } from '../services/UserServices.vue';
-const UserService = new UserServices()
+import { AuthServices } from '../services/AuthServices.vue';
+const userService = new UserServices()
+const authServices = new AuthServices()
 const userData: Ref<UserData | null> = ref(null)
 
-  function fetchUserData(): void {
-  UserService.getUserById(1).then((response: any) => {
+const userId = authServices.getUserId()
+
+function fetchUserData(): void {
+  userService.getUserById(userId).then((response: any) => {
     userData.value = response.data
   })
 }
@@ -33,22 +36,12 @@ onMounted(() => {
       <hr class="h-0.5 my-8 bg-gray-200 border-1 dark:bg-gray-700" />
     </div>
     <div class="mt-4" @click="$emit('showTaskSidebar', true)">
-      <button
-        type="button"
+      <button type="button"
         class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200 rounded-xl dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
-        data-accordion-target="#accordion-collapse-body-1"
-        aria-expanded="true"
-        aria-controls="accordion-collapse-body-1"
-      >
+        data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
         <span>{{ $t('app.todolist.add-task-button') }}</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+          class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
         </svg>
       </button>
